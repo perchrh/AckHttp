80b590bdcac1829644fb0019a3d31dcfc9fbff88
Per Christian Henden <perhenden@gmail.com>
2016-11-05 12:56:54 +0100

    Swift3 update

--- AckHttp.swift
+++ AckHttp.swift
@@ -2,51 +2,63 @@ import Foundation
 
 // Based on example from https://thatthinginswift.com/write-your-own-api-clients-swift/
 
-class AckHttp {
+final class AckHttp {
 
-    private func makeAsyncHttpRequest(request: NSMutableURLRequest, method: String, data: NSData?, headers: [String:String]?, completion: (success:Bool, responseBody:NSData?, errorObject:AnyObject?) -> ()) {
+    fileprivate func makeAsyncHttpRequest(request: NSMutableURLRequest, method: String, data: Data?, headers: [String:String]?, completion: @escaping (_ success:Bool, _ responseBody:Data?, _ errorObject:Error?) -> ()) {
+        if (request.httpBody != nil || request.allHTTPHeaderFields != nil) {
+            print("Warning, the body and headers set by the request will be overridden by " +
+                    "the method parameters.")
+        }
 
-        self.addDataAndHeaders(request, method: method, data: data, headers: headers)
+        self.addDataAndHeaders(request: request, method: method, data: data, headers: headers)
 
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-
-        session.dataTaskWithRequest(request) {
-            (data, response, error) -> Void in
-            if let response = response as? NSHTTPURLResponse where response.statusCode / 100 == 2 {
-                completion(success: true, responseBody: data, errorObject: error)
+        let session = URLSession.shared
+        session.dataTask(with: request as URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let response = response as? HTTPURLResponse , response.statusCode / 100 == 2 {
+                completion(true, data, error)
             } else {
-                completion(success: false, responseBody: data, errorObject: error)
+                print("Error, received response code", (response as? HTTPURLResponse)?.statusCode ?? "unknown")
+                if let data = data, let errorBody = String(data: data, encoding: String.Encoding.utf8) {
+                    print("Error response was", errorBody)
+                }
+                completion(false, data, error)
             }
         }.resume()
+
     }
 
-    private func makeSyncHttpRequest(request: NSMutableURLRequest, method: String, data: NSData?, headers: [String:String]?) -> (success:Bool, responseBody:NSData?, errorObject:AnyObject?) {
+    fileprivate func makeSyncHttpRequest(request: NSMutableURLRequest, method: String, data: Data?, headers: [String:String]?) -> (success:Bool, responseBody:Data?, errorObject:Error?) {
+        self.addDataAndHeaders(request: request, method: method, data: data, headers: headers)
 
-        self.addDataAndHeaders(request, method: method, data: data, headers: headers)
+        let session = URLSession.shared
 
-        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
 
-        let semaphore: dispatch_semaphore_t = dispatch_semaphore_create(0)
-
-        var retval: (success:Bool, responseBody:NSData?, errorObject:AnyObject?)? = nil
-        session.dataTaskWithRequest(request) {
-            (data, response, error) -> Void in
-            if let response = response as? NSHTTPURLResponse where response.statusCode / 100 == 2 {
-                retval = (success: true, responseBody: data, errorObject: error)
+        var retval: (success:Bool, responseBody:Data?, errorObject:Error?)? = nil
+        session.dataTask(with: request as URLRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let response = response as? HTTPURLResponse , response.statusCode / 100 == 2 {
+                retval = (true, data, error)
             } else {
-                retval = (success: false, responseBody: data, errorObject: error)
+                retval = (false, data, error)
+
+                print("Error, received response code", (response as? HTTPURLResponse)?.statusCode ?? "unknown")
+
+                if let data = data, let errorBody = String(data: data, encoding: String.Encoding.utf8) {
+                    print("Error response was", errorBody)
+                }
+
             }
 
-            dispatch_semaphore_signal(semaphore); // mark as done, no more wait
+            semaphore.signal(); // mark as done, no more wait
         }.resume()
 
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        semaphore.wait(timeout: DispatchTime.distantFuture)
         return retval!
     }
 
-    private func addDataAndHeaders(request: NSMutableURLRequest, method: String, data: NSData?, headers: [String:String]?) {
-        request.HTTPMethod = method
-        request.HTTPBody = data
+    fileprivate func addDataAndHeaders(request: NSMutableURLRequest, method: String, data: Data?, headers: [String:String]?) {
+        request.httpMethod = method
+        request.httpBody = data
 
         if let headers = headers {
             for (field, value) in headers {
@@ -55,65 +67,80 @@ class AckHttp {
         }
     }
 
-    func postAsync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil, completion: (success:Bool, responseBody:NSData?, errorObject:AnyObject?) -> ()) {
-        makeAsyncHttpRequest(request, method: "POST", data: data, headers: headers, completion: completion)
+    // Async methods
+    func postAsync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil, completion: @escaping (_ success:Bool, _ responseBody:Data?, _ errorObject:Error?) -> ()) {
+        makeAsyncHttpRequest(request: request, method: "POST", data: data, headers: headers, completion: completion)
     }
 
-    func putAsync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil, completion: (success:Bool, responseBody:NSData?, errorObject:AnyObject?) -> ()) {
-        makeAsyncHttpRequest(request, method: "PUT", data: data, headers: headers, completion: completion)
+    func putAsync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil, completion: @escaping (_ success:Bool, _ responseBody:Data?, _ errorObject:Error?) -> ()) {
+        makeAsyncHttpRequest(request: request, method: "PUT", data: data, headers: headers, completion: completion)
     }
 
-    func getAsync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil, completion: (success:Bool, responseBody:NSData?, errorObject:AnyObject?) -> ()) {
-        makeAsyncHttpRequest(request, method: "GET", data: data, headers: headers, completion: completion)
+    func getAsync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil, completion: @escaping (_ success:Bool, _ responseBody:Data?, _ errorObject:Error?) -> ()) {
+        makeAsyncHttpRequest(request: request, method: "GET", data: data, headers: headers, completion: completion)
     }
 
-    func postSync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:NSData?, errorObject:AnyObject?) {
-        return makeSyncHttpRequest(request, method: "POST", data: data, headers: headers)
+    func postSync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:Data?, errorObject:Error?) {
+        return makeSyncHttpRequest(request: request, method: "POST", data: data, headers: headers)
     }
 
-    func putSync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:NSData?, errorObject:AnyObject?) {
-        return makeSyncHttpRequest(request, method: "PUT", data: data, headers: headers)
+    func putSync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:Data?, errorObject:Error?) {
+        return makeSyncHttpRequest(request: request, method: "PUT", data: data, headers: headers)
     }
 
-    func getSync(request: NSMutableURLRequest, data: NSData? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:NSData?, errorObject:AnyObject?) {
-        return makeSyncHttpRequest(request, method: "GET", data: data, headers: headers)
+    func getSync(request: NSMutableURLRequest, data: Data? = nil, headers: [String:String]? = nil) -> (success:Bool, responseBody:Data?, errorObject:Error?) {
+        return makeSyncHttpRequest(request: request, method: "GET", data: data, headers: headers)
     }
 
-    func createUrl(baseUrl: NSURL, pathComponents: [String], queryParams: Dictionary<String, String>? = nil) -> NSURL {
+    func createUrl(baseUrl: URL, pathComponents: [String], queryParams: Dictionary<String, String>? = nil) -> URL {
         var request = baseUrl
 
-        let lastIndex = pathComponents.count.predecessor()
-        for (index, path) in pathComponents.enumerate() {
+        let lastIndex = (pathComponents.count - 1)
+        for (index, path) in pathComponents.enumerated() {
             let isNotLast = index != lastIndex
-            request = request.URLByAppendingPathComponent(path, isDirectory: isNotLast)
+            request = request.appendingPathComponent(path, isDirectory: isNotLast)
         }
 
-        let result = NSURLComponents(URL: request, resolvingAgainstBaseURL: true)
+        var result = URLComponents(url: request, resolvingAgainstBaseURL: true)
         if let params = queryParams {
-            var queryParameters = [NSURLQueryItem]();
+            var queryParameters = [URLQueryItem]();
             for (name, value) in params {
-                queryParameters.append(NSURLQueryItem(name: name, value: value))
+                queryParameters.append(URLQueryItem(name: name, value: value))
             }
 
             result!.queryItems = queryParameters
         }
 
-        return result!.URL!
+        return result!.url!
+    }
+
+    func createFormDataFromFields(formFields:[String:String]) -> Data {
+        var postData = ""
+        var isFirst = true;
+        for (key, value) in formFields {
+            let prefix = isFirst ? "" : "&"
+            let encodedKey:String = key.stringByAddingPercentEncodingForFormData(true)!
+            let encodedValue:String = value.stringByAddingPercentEncodingForFormData(true)!
+            postData += "\(prefix)\(encodedKey)=\(encodedValue)"
+            isFirst = false
+        }
+
+        return postData.data(using: String.Encoding.utf8)!
     }
 
     // example usage
-    func dummyAsyncServiceCall(email: String, password: String, completion: (success:Bool, data:NSData?, error:AnyObject?) -> ()) {
+    func dummyAsyncServiceCall(_ email: String, password: String, completion: @escaping (_ success:Bool, _ data:Data?, _ error:AnyObject?) -> ()) {
         let queryParameters = ["email": email, "password": password]
         let pathComponents = ["auth", "local"]
-        let baseUrl = NSURL(string: "http://api.mydomain.com:8074/")!
+        let baseUrl = URL(string: "http://api.mydomain.com:8074/")!
 
-        let url = createUrl(baseUrl, pathComponents: pathComponents, queryParams: queryParameters)
-        let request = NSMutableURLRequest(URL: url)
-        postAsync(request) { (success, data, error) -> Void in
+        let url = createUrl(baseUrl:baseUrl, pathComponents: pathComponents, queryParams: queryParameters)
+        let request = NSMutableURLRequest(url: url)
+        postAsync(request: request) { (success, data, error) -> Void in
             if success {
-                completion(success: true, data: data, error: error)
+                completion(true, data, error as AnyObject?)
             } else {
-                completion(success: false, data: data, error: error)
+                completion(false, data, error as AnyObject?)
             }
         }
     }
